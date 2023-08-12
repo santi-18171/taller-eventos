f53d6920c7fff314bbc6807935eec15e15d8d184
santi-18171 <santimartinez18171920@gmail.com>
2023-08-12 01:26:30 -0300

    solucion click

--- eventos.js
+++ eventos.js
@@ -3,3 +3,8 @@ const divContenedor = document.querySelector(".contenedor");
 divContenedor.addEventListener("click", () => {
     alert("Hola! Soy el div");
 });
+
+function saludar(event) {
+    event.stopPropagation();
+    alert("HOLA");
+} 
